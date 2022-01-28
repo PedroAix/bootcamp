1d4f5acff2d8d47672cdef70149198665b13db15
PedroAix <pedroaixvinaroz@hotmail.com>
2022-01-28 10:36:23 +0100

    Ejercicio1

--- Backend/Ejercicios1/src/com/capgemini/Principal.java
+++ Backend/Ejercicios1/src/com/capgemini/Principal.java
@@ -4,28 +4,27 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Principal {
-	
-	public static void MostrarIntentos(int a) {//MUESTRA LOS INTENTOS
+
+	public static void MostrarIntentos(int a) {// MUESTRA LOS INTENTOS
 		System.out.println("INTENTO Nº" + a);
 	}
-	
-	public static void TamañoNumero(int num_aleatorio, int num) {//MUESTRA SI EL NÚMERO ES MÁS GRANDE O PEQUEÑO
-		if (num > num_aleatorio) System.out.println("El número es más pequeño\n");
-		else System.out.println("El número es más grande\n");
+
+	public static void AproximacionNumero(int num_aleatorio, int num) {// MUESTRA SI EL NÚMERO ES MÁS GRANDE O PEQUEÑO
+		if (num > num_aleatorio)
+			System.out.println("El número es más pequeño\n");
+		else
+			System.out.println("El número es más grande\n");
 	}
-	
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner teclado = new Scanner(System.in);
 		var rnd = new Random();
-		
+
 		System.out.println("Adivina el número!\nTienes 10 intentos\n\n");
 		int num_aleatorio = rnd.nextInt(100);
 		int intento = 1;
-		
-		
+
 		do {
 			MostrarIntentos(intento);
 			System.out.print("Introduce un número del 1 al 100: ");
@@ -33,13 +32,12 @@ public class Principal {
 			if (num_aleatorio == num) {
 				System.out.println("CORRECTO!");
 				break;
-			}
-			else {
+			} else {
 				System.out.println("**INCORRECTO**");
 				intento++;
-				TamañoNumero(num_aleatorio,num);
+				AproximacionNumero(num_aleatorio, num);
 			}
-		} while (intento <= 10); //CONDICION DE PERMANENCIA
+		} while (intento <= 10); // CONDICION DE PERMANENCIA
 	}
 
 }
