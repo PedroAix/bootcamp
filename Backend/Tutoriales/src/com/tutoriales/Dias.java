8b9811a1a22dfb8dfb44e695d691b315618241bf
PedroAix <pedroaixvinaroz@hotmail.com>
2022-02-04 11:44:56 +0100

    Ejemplo Enumerado

---
+++ Backend/Tutoriales/src/com/tutoriales/Dias.java
@@ -0,0 +1,15 @@
+package com.tutoriales;
+
+public class Dias {
+
+	public static void main(String[] args) {
+		DiasLaborales d = DiasLaborales.JUEVES;
+		
+		int i = d.getValue();
+		System.out.println(i);
+		
+		 d = DiasLaborales.getEnum(1);
+		 System.out.println(d);
+	}
+
+}
---
+++ Backend/Tutoriales/src/com/tutoriales/DiasLaborales.java
@@ -0,0 +1,31 @@
+package com.tutoriales;
+
+public enum DiasLaborales {
+	LUNES(1), MARTES(2), MIERCOLES(3), JUEVES(4), VIERNES(5);
+	
+	
+	private int value;
+	
+	
+	private DiasLaborales(int value){
+		this.value = value;
+	}
+	
+	
+	public int getValue() {
+		return value;
+	}
+	
+	
+	public static DiasLaborales getEnum(int value) {
+		switch (value) {
+		case 1: return DiasLaborales.LUNES;
+		case 2: return DiasLaborales.MARTES;
+		case 3: return DiasLaborales.MIERCOLES;
+		case 4: return DiasLaborales.JUEVES;
+		case 5: return DiasLaborales.VIERNES;
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + value);
+		}
+	}
+}
