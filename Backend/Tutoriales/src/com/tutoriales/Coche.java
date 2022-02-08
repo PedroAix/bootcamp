08628d3274316870aa45849a1ababe8018864cb8
PedroAix <pedroaixvinaroz@hotmail.com>
2022-02-08 21:26:46 +0100

    Ejercicio Coche

---
+++ Backend/Tutoriales/src/com/tutoriales/Coche.java
@@ -0,0 +1,18 @@
+package com.tutoriales;
+
+public class Coche {
+
+	int ruedasCoche, largoCoche, 
+			anchoCoche, motorCoche, pesoCoche; 
+
+	
+	public Coche() {
+		ruedasCoche = 4;
+		largoCoche = 2000;
+		anchoCoche = 100;
+		motorCoche = 1600;
+		pesoCoche = 500;
+	}
+	
+	
+}
---
+++ Backend/Tutoriales/src/com/tutoriales/UsoCoche.java
@@ -0,0 +1,12 @@
+package com.tutoriales;
+
+public class UsoCoche {
+	
+	public static void main(String[] args) {
+		
+	Coche renault = new Coche(); //INSTANCIAR UNA CLASE. EJEMPLAR DE CLASE
+								 //Nombre_clase nombre = new Nombre_Constructor
+	System.out.println("Este coche tiene " + renault.ruedasCoche + " ruedas");
+	}
+
+}
