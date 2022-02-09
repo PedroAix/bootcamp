66589096e12b5e9ffeae633ff68665b27cefdcad
PedroAix <pedroaixvinaroz@hotmail.com>
2022-02-09 17:52:14 +0100

    Ejercicios

--- Backend/Tutoriales/src/com/tutoriales/Coche.java
+++ Backend/Tutoriales/src/com/tutoriales/Coche.java
@@ -2,12 +2,10 @@ package com.tutoriales;
 
 public class Coche {
 
-	private int ruedasCoche, largoCoche, 
-			anchoCoche, motorCoche, pesoCoche, pesoTotalCoche;
+	private int ruedasCoche, largoCoche, anchoCoche, motorCoche, pesoCoche, pesoTotalCoche;
 	private String colorCoche;
 	private boolean asientosCuero, climatizador;
 
-	
 	public Coche() {
 		ruedasCoche = 4;
 		largoCoche = 2000;
@@ -15,41 +13,39 @@ public class Coche {
 		motorCoche = 1600;
 		pesoCoche = 300;
 	}
-	
+
 	public String getDatos() {
-		return "La plataforma del vehículo tiene " + ruedasCoche
-				+ " ruedas\nMide " + largoCoche/100 + " metros\n"
-						+ "Tiene un ancho de " + anchoCoche/100 +
-						" metros";
+		return "La plataforma del vehículo tiene " + ruedasCoche + " ruedas\nMide " + largoCoche / 100 + " metros\n"
+				+ "Tiene un ancho de " + anchoCoche / 100 + " metros";
 	}
-	
+
 	public String getLargo() {
 		return "Largo del coche: " + largoCoche + " cm";
 	}
-	
-	public int getRuedas() {//GETTER, DEVUELVE UN VALOR CUANDO ES PRIVATE
+
+	public int getRuedas() {// GETTER, DEVUELVE UN VALOR CUANDO ES PRIVATE
 		return ruedasCoche;
 	}
-	
+
 	public String getColor() {
-		return "Color del coche: " + colorCoche;	
+		return "Color del coche: " + colorCoche;
 	}
-	
-	public String getAsientos(){
-		if(asientosCuero) {
+
+	public String getAsientos() {
+		if (asientosCuero) {
 			return "El coche tiene asientos de cuero";
 		}
 		return "El coche tiene asientos de serie";
 	}
-	
-	public void setRuedas(int valor) {//SETTER, ESTABLECE UN VALOR CUANDO ES PRIVATE
+
+	public void setRuedas(int valor) {// SETTER, ESTABLECE UN VALOR CUANDO ES PRIVATE
 		ruedasCoche = valor;
 	}
-	
+
 	public void setColor(String color) {
 		colorCoche = color;
 	}
-	
+
 	public void setAsientos(String asientosCuero) {
 		if (asientosCuero.equalsIgnoreCase("si")) {
 			this.asientosCuero = true;
@@ -57,10 +53,11 @@ public class Coche {
 		else {
 			this.asientosCuero = false;
 		}
-		 //EL OPERADOR THIS SE USA CUANDO Lthis.asientosCuero = asientosCuero;A VARIABLE DE CLASE
-											//TIENE EL MISMO NOMBRE QUE LA VARIABLE DEL PARÁMETRO.
-											//CON EL THIS INDICAMOS LA VARIABLE DE LA CLASE
-	}
+		/**
+		  * EL OPERADOR THIS SE USA CUANDO LA VARIABLE DE CLASE TIENE EL MISMO NOMBRE 
+		  * QUE LA VARIABLE DEL PARÁMETRO.
+		  * CON EL THIS INDICAMOS CUÁL ES LA VARIABLE DE LA CLASE
+		  */
 	
-	
+	}	 
 }
