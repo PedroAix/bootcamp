b72955e5931b3215bee12a506f9e341130380574
PedroAix <pedroaixvinaroz@hotmail.com>
2022-02-22 20:33:30 +0100

    Java Spring

---
+++ Backend/demos.spring/src/main/java/com/example/ioc/ServicioImpl.java
@@ -0,0 +1,16 @@
+package com.example.ioc;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServicioImpl implements Servicio {
+
+	public ServicioImpl() {
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Soy el servicio");
+	}
+
+}
