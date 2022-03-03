65a328bfd08649f55a98772b85d91a54111813b8
PedroAix <pedroaixvinaroz@hotmail.com>
2022-03-03 21:37:44 +0100

    Ejercicio Empleados (Muchos conceptos)

--- Backend/Tutoriales/src/com/tutoriales/Empleado/Director.java
+++ Backend/Tutoriales/src/com/tutoriales/Empleado/Director.java
@@ -2,6 +2,7 @@ package com.tutoriales.Empleado;
 
 public class Director extends Jefatura{
 
+	
 	public Director(String nombre, double sueldo, int año, int mes, int dia) {
 		super(nombre, sueldo, año, mes, dia);
 	}
