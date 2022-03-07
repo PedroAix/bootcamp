bef531054623cade664f2b52b62de626184e038d
PedroAix <pedroaixvinaroz@hotmail.com>
2022-03-07 21:50:18 +0100

    Temporizador y clase interna local

--- Backend/Tutoriales/src/com/tutoriales/Temporizador/Hora2.java
+++ Backend/Tutoriales/src/com/tutoriales/Temporizador/Hora2.java
@@ -11,9 +11,9 @@ public class Hora2 {
 
 	public static void main(String[] args) {
 
-		Reloj reloj = new Reloj(3000, true);
+		Reloj reloj = new Reloj(); //Constructor por defecto
 
-		reloj.enMarcha();
+		reloj.enMarcha(3000, true);
 
 		JOptionPane.showMessageDialog(null, "Pulsa Aceptar para terminar");
 
@@ -24,35 +24,29 @@ public class Hora2 {
 
 class Reloj {
 
-	private int intervalo;
-	private boolean sonido;
+	public void enMarcha(int intervalo, boolean sonido) {
+		class Temporizador2 implements ActionListener {
+			//CLASE INTERNA LOCAL, clase dentro de un método (como solo se utilizan una vez se añaden al método simplificando el código)
+			//No debe llevar ningún modificador de acceso
+			//Clase totalmente encapsulada
+			//Puede acceder a las variables del método y de la clase padre, pero la clase externa no puede acceder a la interna
+			
+			public void actionPerformed(ActionEvent evento) {
 
-	public Reloj(int intervalo, boolean sonido) {
+				Date ahora = new Date();
 
-		this.intervalo = intervalo;
-		this.sonido = sonido;
+				System.out.println("Hora cada " + intervalo + " ms: " + ahora);
 
-	}
-
-	public void enMarcha() {
+				if (sonido)
+					Toolkit.getDefaultToolkit().beep();
+			}
+		}
+		
 		ActionListener listener = new Temporizador2();
 
 		Timer temporizador = new Timer(intervalo, listener);
 
 		temporizador.start();
-	}
-
-	private class Temporizador2 implements ActionListener {//CLASE INTERNA (Puede acceder a los datos de la clase que la engloba, aunque sean privados)
-
-		public void actionPerformed(ActionEvent evento) {
-
-			Date ahora = new Date();
-
-			System.out.println("Hora cada " + intervalo + " ms: " + ahora);
-
-			if (sonido)
-				Toolkit.getDefaultToolkit().beep();
-		}
 	}
 
 }
