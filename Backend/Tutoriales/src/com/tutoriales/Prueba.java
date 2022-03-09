7fdc7c34c478b30df88564c8ad73bb3211969f3d
PedroAix <pedroaixvinaroz@hotmail.com>
2022-03-09 11:57:27 +0100

    Tutoriales y pruebas

--- Backend/Tutoriales/src/com/tutoriales/Prueba.java
+++ Backend/Tutoriales/src/com/tutoriales/Prueba.java
@@ -2,38 +2,61 @@ package com.tutoriales;
 
 
 public class Prueba {
-	
-	public static void main (String[] args) {
-	
-		Wolf l = new Wolf();
+	public static void main(String[] args) {
 		
-		System.out.println(l.makeNoise(false));
+		Meses mes1 = Meses.JULIO;
+		System.out.println(mes1);
+		System.out.println("Numero: " + mes1.getNumeroMes());
+		
+		System.out.println("----------------------------------------");
+		
+		int mes2 = 3;
+		System.out.println(Meses.getNombreMes(mes2));
+		 
+		 
 		
 		
 	}
-	
-	
-	
-}
 
-
-class Dog { 
-    String makeNoise(boolean isAngry) { 
-        if (isAngry) { 
-            return "Grrrrr!"; 
-        } else { 
-            return "Woof!"; 
-        } 
-    } 
-} 
- 
-class Wolf extends Dog { 
-	@Override 
-    String makeNoise(boolean isAngry) { 
-        if (isAngry) { 
-            return "Grrrrr!"; 
-        } else { 
-            return "Aaoouuuuuuuu!"; 
-        } 
-    } 
+	enum Meses {
+		ENERO(1), FEBRERO(2), MARZO(3), ABRIL(4), MAYO(5), JUNIO(6), JULIO(7), AGOSTO(8), 
+		SEPTIEMBRE(9), OCTUBRE(10), NOVIEMBRE(11), DICIEMBRE(12);
+		
+		private int numeroMes;
+		
+		
+		private Meses(int numeroMes) {
+			this.numeroMes = numeroMes;
+		}
+		
+		
+		
+		public int getNumeroMes() {
+			
+			return numeroMes;
+		}
+		
+		public static Meses getNombreMes(int numeroMes) {
+			switch(numeroMes) {
+			case 1: return Meses.ENERO;
+			case 2: return Meses.FEBRERO;
+			case 3: return Meses.MARZO;
+			case 4: return Meses.ABRIL;
+			case 5: return Meses.MAYO;
+			case 6: return Meses.JUNIO;
+			case 7: return Meses.JULIO;
+			case 8: return Meses.AGOSTO;
+			case 9: return Meses.SEPTIEMBRE;
+			case 10: return Meses.OCTUBRE;
+			case 11: return Meses.NOVIEMBRE;
+			case 12: return Meses.DICIEMBRE;
+			default:
+				throw new IllegalArgumentException("Numero de mes no válido");
+			}
+			
+		}
+		
+		
+		
+	}
 }
--- Backend/Tutoriales/src/com/tutoriales/Prueba2.java
+++ Backend/Tutoriales/src/com/tutoriales/Prueba2.java
@@ -1,8 +1,77 @@
 package com.tutoriales;
 
 
-public class Tutorial2 {
+public class Prueba2 {
 
 	public static void main(String[] args) {
+	
+		Cuenta cuenta1 = new Cuenta("Pedro Aix Vinaroz", 2000);
+		Cuenta cuenta2 = new Cuenta("Mariola Navarro Marín");
+		
+		System.out.println(cuenta1.toString());
+		System.out.println();
+		System.out.println(cuenta2.toString());
+		
+		cuenta1.retirarCantidad(2100);
+		System.out.println(cuenta1.toString());
+		cuenta1.ingresarCantidad(-4);
+	}
+	
+	
+}
+
+
+class Cuenta {
+	
+	private String titular;
+	private double cantidad;
+	
+	
+	public Cuenta(String titular) {
+		this.titular = titular;
+	}
+	
+	public Cuenta(String titular, double cantidad) {
+		this.titular = titular;
+		this.cantidad = cantidad;
+	}
+	
+	
+	public void ingresarCantidad(double cantidad) {
+		if (cantidad > 0) 
+			this.cantidad += cantidad;
+		else
+			throw new IllegalArgumentException("La cantidad debe ser mayor a 0");
+	}
+	
+	public void retirarCantidad(double cantidad) {
+		if (this.cantidad - cantidad < 0)
+			this.cantidad = 0;
+		
+		else
+			this.cantidad -= cantidad;
+		
+	}
+	
+	
+	public String getTitular() {
+		return titular;
+	}
+	
+	public double getcantidad() {
+		return cantidad;
+	}
+	
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+	
+	public void setCantidad(double amount) {
+		cantidad = amount;
+	}
+	
+	@Override
+	public String toString() {
+		return "Titular: "+ titular + "\nCantidad: " + cantidad;
 	}
 }
