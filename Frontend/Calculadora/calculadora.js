fb2180cb53aa4feaf7880f58a5fe7047b610a90f
PedroAix <pedroaixvinaroz@hotmail.com>
2022-03-20 18:28:31 +0100

    Calculadora terminada

--- Frontend/Calculadora/calculadora.js
+++ Frontend/Calculadora/calculadora.js
@@ -6,13 +6,13 @@ class Calculadora {
   }
 
   inicializar() {
-    this.total = 0;
+    this.acumulado = 0;
     this.operadorPendiente = "+";
     this.sustituir = false;
+    this.igual = false;
   }
 
   ponerDigito(digito) {
-    //CONSEGUIR QUE LOS NÚMEROS NO SE SALGAN DE LA PANTALLA
     if (this.pantalla.textContent == "0") {
       this.pantalla.textContent = digito;
     } else {
@@ -21,27 +21,36 @@ class Calculadora {
   }
 
   ponerHistorial() {
-    this.historial.textContent = this.getTotal() + " " + this.operadorPendiente;
+    this.historial.textContent =
+      this.getAcumulado() + " " + this.operadorPendiente;
+  }
+
+  ponerHistorialResultado(acumulado,operador,operando,igual) {
+    return acumulado + " " + operador + " " + operando + " " + igual;
   }
 
   ponerDecimal() {
-    if (this.sustituir) {
-      this.borrarPantalla();
-      this.sustituir = false;
-    } else {
-      if (this.pantalla.textContent.match(/\d+\.\d*/))
-        throw Error("Ya existe un valor decimal");
+    if (!this.igual) {
+      if (this.sustituir) {
+        this.borrarPantalla();
+        this.sustituir = false;
+      } else {
+        if (this.pantalla.textContent.match(/\d+\.\d*/))
+          throw Error("Ya existe un valor decimal");
+      }
+      this.pantalla.textContent += ".";
     }
-    this.pantalla.textContent += ".";
   }
 
   cambiarSigno() {
     if (this.pantalla.textContent == "0")
       throw Error("No se puede cambiar de signo al 0");
-    if (this.pantalla.textContent.match(/^-/)) {
-      this.pantalla.textContent = this.pantalla.textContent.replace(/^-/, "");
-    } else {
-      this.pantalla.textContent = "-" + this.pantalla.textContent;
+    if (!this.igual) {
+      if (this.pantalla.textContent.match(/^-/)) {
+        this.pantalla.textContent = this.pantalla.textContent.replace(/^-/, "");
+      } else {
+        this.pantalla.textContent = "-" + this.pantalla.textContent;
+      }
     }
   }
 
@@ -76,29 +85,24 @@ class Calculadora {
     this.pantalla.textContent = "0";
   }
 
-  getTotal() {
-    return this.total;
+  getAcumulado() {
+    return this.acumulado;
   }
 
   calcula(operando2, nuevoOperador) {
     this.operando = operando2;
-    //console.log(operando2 + " " + nuevoOperador);
-
     switch (this.operadorPendiente) {
       case "+":
-        this.total += operando2;
+        this.acumulado += operando2;
         break;
       case "-":
-        this.total -= operando2;
+        this.acumulado -= operando2;
         break;
       case "x":
-        this.total *= operando2;
+        this.acumulado *= operando2;
         break;
       case "/":
-        this.total /= operando2;
-        break;
-      case "%":
-        this.total %= operando2;
+        this.acumulado /= operando2;
         break;
       case "=":
         break;
@@ -106,6 +110,6 @@ class Calculadora {
         throw new Error("Operación no soportada.");
     }
     this.operadorPendiente = nuevoOperador;
-    return this.getTotal();
+    return this.getAcumulado();
   }
 }
