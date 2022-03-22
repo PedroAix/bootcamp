8a815bdfd08ebce65a0dde063ebe7688c0e0665a
PedroAix <pedroaixvinaroz@hotmail.com>
2022-03-22 18:10:47 +0100

    Laboratorio Angular

---
+++ Angular/src/app/demos/demos.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  NotificationService,
+  NotificationType,
+} from '../common-services/notification.service';
+
+import { Unsubscribable } from 'rxjs';
+
+@Component({
+  selector: 'app-demos',
+  templateUrl: './demos.component.html',
+  styleUrls: ['./demos.component.css'],
+})
+export class DemosComponent implements OnInit, OnDestroy {
+  private suscriptor: Unsubscribable | undefined;
+
+  constructor(public vm: NotificationService) {}
+
+  ngOnInit(): void {
+    this.suscriptor = this.vm.Notificacion.subscribe((n) => {
+      if (n.Type !== NotificationType.error) {
+        return;
+      }
+      window.alert(`Suscripcion: ${n.Message}`);
+      this.vm.remove(this.vm.Listado.length - 1);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.suscriptor) {
+      this.suscriptor.unsubscribe();
+    }
+  }
+}
