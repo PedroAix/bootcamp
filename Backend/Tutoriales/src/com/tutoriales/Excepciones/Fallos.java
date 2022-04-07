9e5f225f663c692b29ac5799e00b775ce21f0d65
PedroAix <pedroaixvinaroz@hotmail.com>
2022-04-07 17:15:13 +0200

    Tutorial de excepciones

--- Backend/Tutoriales/src/com/tutoriales/Excepciones/Fallos.java
+++ Backend/Tutoriales/src/com/tutoriales/Excepciones/Fallos.java
@@ -3,6 +3,28 @@ package com.tutoriales.Excepciones;
 import javax.swing.JOptionPane;
 
 public class Fallos {
+	
+	//Los errores pueden ser heredados de:
+	//IOException, (excepcion comprobada) RuntimeException (excepción no comprobada)
+	
+	//Los errores heredados de IOException deben ser usados con el
+	//código try{]catch(){}. Son errores que no tienen que ver con el programador
+	
+	//Los errores heredados de RuntimeException son errores de código
+	//producidos por el programador. Se solucionan depurando mejor el código
+	//No requieren uso del try catch pero lo podemos usar del mismo modo
+	
+	//Podemos usar en la firma del método throws Error_Generado, pero
+	//no está muy bien visto
+	
+	
+	//Si al llamar un método puede generar un error lo mejor es capturarlo 
+	//envolviendo la llamada del método con el try catch y definiendo el throws
+	//en la firma del método
+	
+	//Tanto RuntimeException como IOException heredan de la misma clase: Exception
+	//Utilizando Exception en try catch o en el throws abarcamos ambos tipos de errores
+	//de una forma más general
 
 
 	public static void main(String[] args) {
@@ -13,6 +35,13 @@ public class Fallos {
 		matriz[2] = -15;
 		matriz[3] = 92;
 		matriz[4] = 71;
+		
+		try {
+			matriz[5] = 1;
+		} catch (Exception e) {
+			System.out.println("Se ha genereado un error");
+		}
+		 
 		
 		
 		for(int i = 0; i < matriz.length; i++) {
